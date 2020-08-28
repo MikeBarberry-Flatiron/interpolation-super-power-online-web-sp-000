1385149a8141cbf81590329bda529c09140a7757
Mike Barberry <mikebarberry@protonmail.com>
2020-08-28 22:47:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,5 +4,3 @@ colors = ["red", "orange", "yellow", "blue", "indigo", "violet"]
 def display_rainbow(array)
   puts "R: #{array[0]}, O: #{array[1]}, Y:#{array[2]}, B: #{array[3]}, I: #{array[4]}, V: #{array[5]}"
 end
-
-d
